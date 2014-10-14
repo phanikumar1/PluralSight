7b917ab82af97f604c3271f4e98a52d3bd675751
phanikumar1 <mailphanibalusu@gmail.com>
2014-10-14 10:26:33 -0600

    Simple Test Case 1

--- PluralSight/PackageInstaller.cs
+++ PluralSight/PackageInstaller.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace PluralSight
 {
 	public class PackageInstaller
 	{
+		public static void Main()
+		{
+
+		}
+
+		public static string GetPackageInstallationOrder(string[] packagesDependencyInputs)
+		{
+			var stack = new Stack();
+			foreach (var packagesDependencyInput in packagesDependencyInputs)
+			{
+				var packages = packagesDependencyInput.Split(':');
+				foreach (var package in packages)
+				{
+					if (package.Length > 0)
+					{
+						stack.Push(package);
+					}
+				}
+			}
+
+			return BuildOutputString(stack);
+		}
+
+		private static string BuildOutputString(Stack stack)
+		{
+			if (stack.Count == 0)
+			{
+				return null;
+			}
+			var outputString = string.Empty;
+			var totalCount = stack.Count;
+			for (int count = 0; count < totalCount; count++)
+			{
+				if (!outputString.Contains(stack.Peek().ToString()))
+				{
+					outputString += "," + stack.Pop();
+				}
+				else
+				{
+					stack.Pop();
+				}
+			}
+			return outputString.Substring(1, outputString.Length-1);
+		}
 	}
 }
--- PluralSightTestProject/PackageInstallerTests.cs
+++ PluralSightTestProject/PackageInstallerTests.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluralSight;
 
 namespace PluralSightTestProject
 {
 	[TestClass]
 	public class PackageInstallerTests
 	{
+		[TestMethod]
+		public void TestPackageInstallerOrder_SimpleInput_Success()
+		{
+			const string testInput1 = "KittenService:CamelCaser";
+			const string testInput2 = "CamelCaser:";
+			var input = new[] {testInput1, testInput2};
+			var packageOrder = PackageInstaller.GetPackageInstallationOrder(input);
+			Assert.AreEqual("CamelCaser,KittenService", packageOrder);
+		}
 	}
 }
